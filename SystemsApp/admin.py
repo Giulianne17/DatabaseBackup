7a906e1edafec644a780957d21ab6399e2fd872b
Giulianne17 <gvtr96@gmail.com>
2019-10-31 11:15:28 +0100

    Se añadieron los modelos iniciales del módulo de los sistemas.

--- SystemsApp/admin.py
+++ SystemsApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from SystemsApp.models import *
 # Register your models here.
+
+
+class SistemaAdmin(admin.ModelAdmin):
+	list_display =['id_system','name','bd_name','bd_password','bd_username','bd_type','frequency','type_frequency','server_ip','server_username','server_password','server_route_save']
+
+class CopiaRespaldoAdmin(admin.ModelAdmin):
+	list_display =['id_respaldo','id_system','receptor_server_username','receptor_server_ip','receptor_route_save']
+
+# Sistemas
+admin.site.register(Sistema, SistemaAdmin)
+# Servidores donde se guardaran las copias.
+admin.site.register(CopiaRespaldo, CopiaRespaldoAdmin)
+
--- SystemsApp/models.py
+++ SystemsApp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+import re
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class Sistema(models.Model):
+    TYPE_CHOICES = (('mysql', 'Mysql'),
+                   ('postgresql', 'PostgreSQL'))
+    FREQUENCY_CHOICES = (('days', 'Days'),
+                   ('hours', 'Hours'),
+                   ('minutes', 'Minutes'), 
+                   ('seconds', 'Seconds'))
+    id_system=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=200)
+    bd_name=models.CharField(max_length=200)
+    bd_password=models.CharField(max_length=200)
+    bd_username=models.CharField(max_length=200)
+    bd_type= models.CharField(max_length=11, choices=TYPE_CHOICES)
+    frequency=models.IntegerField(validators=[MinValueValidator(1)])
+    type_frequency=models.CharField(max_length=11, choices=FREQUENCY_CHOICES)
+    server_ip=models.CharField(max_length=20,validators=[RegexValidator((re.compile('([0-9]{1,3}.?){4}')), ('ip incorrecta'), 'invalid')])
+    server_username=models.CharField(max_length=200)
+    server_password=models.CharField(max_length=200)
+    server_route_save=models.CharField(max_length=500, blank=True, null=True)
+    
+    def __str__(self):    
+        '''Devuelve el modelo en tipo String'''
+        return str(self.name)
+
+class CopiaRespaldo(models.Model):
+    id_respaldo=models.AutoField(primary_key=True)
+    id_system=models.ForeignKey(Sistema, on_delete=models.CASCADE)
+    receptor_server_username=models.CharField(max_length=200)
+    receptor_server_ip=models.CharField(max_length=20,validators=[RegexValidator((re.compile('([0-9]{1,3}.?){4}')), ('ip incorrecta'), 'invalid')])
+    receptor_route_save=models.CharField(max_length=500)    
+    
+    def __str__(self):    
+        '''Devuelve el modelo en tipo String'''
+        return str(self.id_respaldo)
